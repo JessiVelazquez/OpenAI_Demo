2eb1b1271a3ca80297a39c2409dc759ba6e64e08
Jessi Velazquez <jessivelazq1@gmail.com>
2023-04-11 14:12:44 -0700

    rename

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Jessi's OpenAI API Demo</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
