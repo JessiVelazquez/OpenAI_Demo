f8e3a2534adb5974a7bcabe9d53a062cb2adc5ba
Jessi Velazquez <jessivelazq1@gmail.com>
2023-04-11 13:41:32 -0700

    q

---
+++ pages/api/models.js
@@ -0,0 +1,11 @@
+// Definition object for models available for use with the openAI /completions endpoint
+
+const models = {
+  davinci2: "text-davinci-002",
+  davinci3: "text-davinci-003", // recommended - lowest cost per token!
+  curie1: "text-curie-001",
+  babbage: "text-babbage-001",
+  ada: "text-ada-001"
+};
+
+export default models;
